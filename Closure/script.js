f80bb6978b2b75766e74b5dfe5377e63ced06f0d
Shukra Tamang <shukratamang.npl@gmail.com>
2025-01-05 19:34:32 +1100

    closure

---
+++ Closure/script.js
@@ -0,0 +1,10 @@
+function outer() {
+  let count = 0;
+  return function inner() {
+    count++;
+    return count;
+  };
+}
+const counter = outer();
+console.log(counter()); // 1
+console.log(counter()); // 2
