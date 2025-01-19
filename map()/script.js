4964816993d2e00671b5e613dc310c29ba060f1d
Shukra Tamang <shukratamang.npl@gmail.com>
2025-01-19 22:30:43 +1100

    map method

---
+++ map()/script.js
@@ -0,0 +1,7 @@
+// The Javascript map() method creates a new array by applying a function to each element of an existing array
+
+const numbers = [1, 2, 3, 4];
+
+const doubled = numbers.map((number) => number * 2);
+
+console.log(doubled);
